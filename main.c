7247b1b49d0ca88b1c859c216f8637b53bc01d43
CwbhX <cwbh10@gmail.com>
2019-03-20 01:33:19 -0400

    Fixed bugs in the bufLen function and moved functions to top of file

--- main.c
+++ main.c
@@ -2,10 +2,43 @@
 #include <stdlib.h>
 #include <string.h> /* memset */
 
-char* stringBuff;
+// ---- CONSTANTS ----
+const int BITLENGTH = 32; // 4bits * 8 hex values representing a row each (e.g. one full column)
 
-int main(int argc,char* argv[]) 
-{ 
+
+// ---- FUNCTION DECLARATIONS ----
+void setupStringBuff(char* stringBuff, int messageLength){
+    stringBuff = malloc(BITLENGTH * messageLength); // Allocate memory for our array of bits by taking the number of bits per column and how many columns (width) of text we have
+
+    if (!stringBuff){ // If we could not allocate memory
+        perror("Error allocating memory");
+        abort();
+    }
+    memset(stringBuff, 0, BITLENGTH*messageLength); // Initialise all the data with zeros to prevent erroneous data from lingering
+    return;
+}
+// Used to find the length of the string for initiasation of our string buffer
+int bufLen(char* stringPointer){
+    int length = 0;
+    int count;
+    printf("Length of String: %lu\n", strlen(stringPointer));
+
+    for (count = 0; count < strlen(stringPointer); count++){ // Iterate through the string since the null character method and while loop was not working
+        if(stringPointer[count] == '_'){
+            length += 3; // 3 Pixels of space -- e.g. 3 sets of 8 Zeros in HEX
+        }else{
+            length += 6; // 6 Characters are 6 pixels wide -- 6 sets of 8 HEX values
+        }
+    }
+    return length; // Return length in width of pixels that our string is
+}
+
+
+// ---- MAIN -----
+int main(int argc,char* argv[]) {
+    int colourMap; // Int gives us 4 Bytes of data == 32 Bits which gives us 8 Hex values
+    char* stringBuff;
+
     int counter; 
     printf("Program Name Is: %s",argv[0]); 
     if(argc==1){
@@ -15,27 +48,20 @@ int main(int argc,char* argv[])
         printf("\nNumber Of Arguments Passed: %d",argc); 
         printf("\n----Following Are The Command Line Arguments Passed----"); 
         for(counter=0;counter<argc;counter++) 
-            printf("argv[%d]: %s\n",counter,argv[counter]); 
+            printf("argv[%d]: %s\n",counter,argv[counter]);
+        
+        if (argc == 2){ // If we do not supply a colour mask value, apply all white
+            colourMap = 0xFFFFFFFF;
+        }else{          // Otherwise apply our specified mask value
+            colourMap = (int) argv[3];
+        }
     } 
 
+    int messageLength = bufLen(argv[1]);
+    setupStringBuff(stringBuff, messageLength);
 
+    printf("Message Length: %d\n", messageLength);
+    printf("Colour Mask Value: %d\n", colourMap);
 
     return 0; 
 } 
-
-// Used to find the length of the string for initiasation of our string buffer
-int bufLen(char* stringPointer){
-    int length = 0;
-    int count = 0;
-
-    while (stringPointer != NULL && *stringPointer != '\0'){ // If the pointer passed is not NULL and the value of the pointer is not the ending character
-        if(*stringPointer == '_'){
-            length += 3; // 3 Pixels of space -- e.g. 3 sets of 8 Zeros in HEX
-        }else{
-            length += 8; // 6 Characters are 6 pixels wide -- 6 sets of 8 HEX values
-        }
-        count++;
-    }
-
-    return length;
-}
